3f9bf09aeb4e629f43cabf122af4ff287406a576
Maddie <115709272+Maddily@users.noreply.github.com>
2024-12-16 12:51:19 +0200

    Create a footer
    The footer links to the repo on GitHub, mentions
    the license and gives credit to the owner of
    the background photo.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,6 +11,39 @@ function App() {
           <span className="score">Score: 0</span>
         </div>
       </header>
+      <footer>
+        <p className="repo">
+          View the source code on{' '}
+          <a href="https://github.com/Maddily/memory-card" target="_blank">
+            Github
+          </a>
+        </p>
+        <div className="photo-credit">
+          Background photo by{' '}
+          <a
+            href="https://unsplash.com/@iangvalerio?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash"
+            target="_blank"
+          >
+            Ian Valerio
+          </a>{' '}
+          on{' '}
+          <a
+            href="https://unsplash.com/photos/aerial-view-photography-of-road-between-highrise-building-CAFq0pv9HjY?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash"
+            target="_blank"
+          >
+            Unsplash
+          </a>
+        </div>
+        <p className="license">
+          Licensed under{' '}
+          <a
+            href="https://creativecommons.org/licenses/by-nc-nd/4.0/"
+            target="_blank"
+          >
+            CC BY-NC-ND 4.0
+          </a>
+        </p>
+      </footer>
     </>
   );
 }
