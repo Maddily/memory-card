aec6ab87638e820afdcb33443cd1fb16bf75180e
Maddie <115709272+Maddily@users.noreply.github.com>
2024-12-18 14:33:31 +0200

    Create Card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,19 @@
+import '../styles/Card.css';
+
+/**
+ * A component to render a character card.
+ *
+ * @param {number} id - Character id
+ * @param {string} name - Character name
+ * @param {string} imageUrl - Image url
+ * @param {function(event)} onClick - A function that handles a click on a card
+ * @returns {JSX.Element}
+ */
+export default function Card({ id, name, imageUrl, onClick }) {
+  return (
+    <div onClick={onClick} className="card" data-id={id}>
+      <img src={imageUrl} alt={'Image of' + name} />
+      <p className="character-name">{name}</p>
+    </div>
+  );
+}
