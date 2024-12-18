2d071b2c5c5cbf6f40792cb6ba723caea8c639cc
Maddie <115709272+Maddily@users.noreply.github.com>
2024-12-18 17:54:56 +0200

    Use states for current score and highscore

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,23 @@
+import { useState } from 'react';
 import '../styles/App.css';
 import '../styles/normalize.css';
 
 function App() {
+  const [currentScore, setCurrentScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
+
   return (
     <>
       <header>
         <h1>Ghibli Cards</h1>
         <div className="score">
-          <span className="high-score">Highscore: 0</span>
-          <span className="score">Current Score: 0</span>
+          <span className="high-score">Highscore: {highScore}</span>
+          <span className="score">Current Score: {currentScore}</span>
         </div>
       </header>
       <p className="instructions">
-        Click each card once to earn points!
-        Don&apos;t click the same card twice, and keep an eye on your score!
+        Click each card once to earn points! Don&apos;t click the same card
+        twice, and keep an eye on your score!
       </p>
       <footer>
         <p className="repo">
